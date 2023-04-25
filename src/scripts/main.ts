ce688bb94db8c78a4c44bc33fb7a4bca8dae9dc0
YahorDanchanka <danchenko.dev@yandex.by>
2023-04-25 17:12:12 +0300

    Added header

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,21 @@
+import tippy from 'tippy.js'
+import 'tippy.js/dist/tippy.css'
 import '@/styles/main.sass'
+
+const submenus = document.querySelectorAll('.nav_submenu')
+
+submenus.forEach((submenu) => {
+  const navLink = submenu.previousElementSibling
+  if (!navLink) return
+
+  navLink.addEventListener('click', (e) => {
+    e.preventDefault()
+  })
+
+  tippy(navLink, {
+    content: submenu,
+    allowHTML: true,
+    interactive: true,
+    placement: 'bottom',
+  })
+})
