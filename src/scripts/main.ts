a3c734b55397b0d273612760f2e91cab4e00b277
YahorDanchanka <danchenko.dev@yandex.by>
2023-04-25 20:34:00 +0300

    Added agents

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,6 +1,41 @@
+import Swiper from 'swiper'
+import 'swiper/css'
 import tippy from 'tippy.js'
 import 'tippy.js/dist/tippy.css'
 import '@/styles/main.sass'
+
+new Swiper('.agents__slider', {
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    576: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1200: {
+      slidesPerView: 2.8,
+      spaceBetween: 40,
+    },
+    1400: {
+      slidesPerView: 3.2,
+      spaceBetween: 40,
+    },
+    2000: {
+      slidesPerView: 3.8,
+      spaceBetween: 144,
+    },
+  },
+})
 
 const submenus = document.querySelectorAll('.nav_submenu')
 
