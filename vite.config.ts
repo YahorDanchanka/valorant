e75854fb594865c9be1a46f8d76efbc89ad2b970
YahorDanchanka <danchenko.dev@yandex.by>
2023-04-26 20:20:14 +0300

    Added postcssSortMediaQueries plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import path from 'path'
+import postcssSortMediaQueries from 'postcss-sort-media-queries'
 
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [postcssSortMediaQueries({ sort: 'desktop-first' })],
+    },
+  },
 })
